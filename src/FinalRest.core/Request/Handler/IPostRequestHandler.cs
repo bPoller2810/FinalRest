01f813b0a90a8b4cd6f81b0f95b05a11691aa357
bernhard <bernhard@pollerspoeck.net>
2021-07-09 21:15:58 +0200

    Interface Documentations

--- src/FinalRest.core/Request/Handler/IPostRequestHandler.cs
+++ src/FinalRest.core/Request/Handler/IPostRequestHandler.cs
@@ -4,6 +4,13 @@ namespace FinalRest.core
 {
     public interface IPostRequestHandler
     {
+
+        /// <summary>
+        /// A callback that acts after the request is made
+        /// </summary>
+        /// <typeparam name="TDataType">The type of data that the request is returning</typeparam>
+        /// <param name="statusCode">The StatusCode of this request</param>
+        /// <param name="data">The data that the request is returning</param>
         void HandlePostRequest<TDataType>(HttpStatusCode statusCode, TDataType data);
     }
 }
--- src/FinalRest.core/Request/Handler/IPreRequestHandler.cs
+++ src/FinalRest.core/Request/Handler/IPreRequestHandler.cs
@@ -2,6 +2,12 @@
 {
     public interface IPreRequestHandler
     {
+
+        /// <summary>
+        /// A callback that acts before the request is made.
+        /// Here you can add headers to this request
+        /// </summary>
+        /// <param name="headers"></param>
         void HandlePreRequest(FinalRestHeaderCollection headers);
     }
 }
